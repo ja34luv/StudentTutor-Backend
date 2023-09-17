54e0f3ff7af99e5a2c0114182d3393430c9010e5
Jay <ja34luv@gmail.com>
2023-09-16 19:00:43 -0700

    update

--- src/models/tutorProfile.js
+++ src/models/tutorProfile.js
@@ -14,12 +14,15 @@ const tutorProfileSchema = new mongoose.Schema(
         experiences: {
             type: [
                 {
-                    title: { type: String, required: true },
-                    employmentType: { type: String },
-                    companyName: { type: String, required: true },
-                    location: { type: String },
-                    locationType: { type: String },
-                    startDateMonth: { type: String, required: true },
+                    title: { type: String, required: true, trim: true },
+                    employmentType: { type: String }, //enum
+                    companyName: { type: String, required: true, trim: true },
+                    location: { type: String, trim: true },
+                    locationType: { type: String }, //enum
+                    startDateMonth: {
+                        type: String,
+                        required: true,
+                    },
                     startDateYear: { type: Number, required: true },
                     endDateMonth: { type: String },
                     endDateYear: { type: Number },
@@ -49,22 +52,79 @@ const tutorProfileSchema = new mongoose.Schema(
         education: {
             type: [
                 {
-                    school: { type: String, required: true },
-                    degree: { type: String },
-                    major: { type: String, required: true },
+                    school: { type: String, required: true, trim: true },
+                    degree: { type: String, trim: true },
+                    major: { type: String, required: true, trim: true },
                     startDateMonth: { type: String, required: true },
                     startDateYear: { type: Number, required: true },
                     endDateMonth: { type: String, required: true },
                     endDateYear: { type: Number, required: true },
-                    gpa: { type: Number },
+                    gpa: { type: Number, trim: true },
                 },
             ],
             validate: {
                 validator: function (education) {
                     if (!education.length > 0)
-                        throw new Error("Education information is needed.");
+                        throw new Error(
+                            "Please provide an education information."
+                        );
                 },
             },
+        },
+        skills: {
+            type: [
+                {
+                    skill: { type: String, required: true, trim: true },
+                },
+            ],
+        },
+        languages: {
+            type: [
+                {
+                    language: { type: String, required: true, trim: true },
+                    proficiency: { type: String },
+                },
+            ],
+            validate: {
+                validator: function (languages) {
+                    if (!languages.length > 0)
+                        throw new Error(
+                            "Please provide a language information."
+                        );
+                },
+            },
+        },
+        hourlyRate: {
+            type: Number,
+            required: true,
+            trim: true,
+        },
+        avatar: { type: Buffer },
+        sex: { type: String, required: true, trim: true }, //enum
+        lessonMethod: { type: String, required: true }, //enum
+        lessonLocation: {
+            type: String,
+            trim: true,
+            required: function () {
+                // Use a function to conditionally set the 'required' property based on lessonMethod
+                return this.lessonMethod !== "remote";
+            },
+        },
+        aboutMe: {
+            type: String,
+            required: true,
+        },
+        aboutLesson: {
+            type: String,
+            required: true,
+        },
+        subjectsOffered: {
+            type: [
+                {
+                    subject: { type: String, trim: true, required: true },
+                    teachingLevels: { type: String, trim: true },
+                },
+            ],
         },
     },
     {
@@ -75,3 +135,7 @@ const tutorProfileSchema = new mongoose.Schema(
 const TutorProfile = mongoose.model("TutorProfile", tutorProfileSchema);
 
 module.exports = TutorProfile;
+
+// notes
+// for lessonMethod & lessonLocation => if lessonMethod is remote, lessonLocation input is disabled from frontend
+//endDateMonth/endDateYear & currentlyWorking => if currentlyWorking is on disable endDateMonth/endDateYear input from frontend
